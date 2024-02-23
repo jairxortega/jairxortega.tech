b301ce46b6eb84f26b87c3e34b7b23cb1d0a70ca
jairxortega <104592853+jairxortega@users.noreply.github.com>
2024-02-23 13:40:58 -0600

    moved components folder to src

--- Refactor/my-app/src/App.tsx
+++ Refactor/my-app/src/App.tsx
@@ -1,27 +1,26 @@
 import React from "react";
 import "./App.css";
-import NavBar from "../components/nav";
+import NavBar from "./components/nav";
 
 function App() {
   return (
-    <>
-      <NavBar>
-        <div className="App">
-          <section id="about">
-            <h2>About</h2>
-            <p>This section contains information about me.</p>
-          </section>
-          <section id="experience">
-            <h2>Experience</h2>
-            <p>This section highlights my work experience.</p>
-          </section>
-          <section id="contact">
-            <h2>Contact</h2>
-            <p>This section provides my contact information.</p>
-          </section>
-        </div>
-      </NavBar>
-    </>
+    <div>
+      <NavBar />
+      <div className="App">
+        <section id="about">
+          <h2>About</h2>
+          <p>This section contains information about me.</p>
+        </section>
+        <section id="experience">
+          <h2>Experience</h2>
+          <p>This section highlights my work experience.</p>
+        </section>
+        <section id="contact">
+          <h2>Contact</h2>
+          <p>This section provides my contact information.</p>
+        </section>
+      </div>
+    </div>
   );
 }
 
