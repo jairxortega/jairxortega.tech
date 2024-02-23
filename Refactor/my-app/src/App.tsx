7f86357c0e1cc97b96d1a5860776acd2a3bfaf76
jairxortega <104592853+jairxortega@users.noreply.github.com>
2024-02-22 19:43:50 -0600

    initial commit

---
+++ Refactor/my-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello World</p>
+        <h2>My name is Jair Ortega</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
